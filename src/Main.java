2b62b5ec6f94f360323b7af03fe332ed0f88ce47
IrinIrin <iraakira1992@gmail.com>
2023-06-22 14:59:52 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 15000;
+        int x = 20;
+        int bonus = ticketPrice / x;
+
+        System.out.println("Ваше количество бонусных миль составляет: " + bonus);
+
+    }
+}
